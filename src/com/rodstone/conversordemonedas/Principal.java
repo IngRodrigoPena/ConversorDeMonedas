43f9af718a6e14eb6d2c71ca99f443eff23b916e
IngRodrigoPena <ingrodrigopena@gmail.com>
2025-05-11 21:49:05 -0600

    prueba del metodo apiClient.sendRequest() ok

--- src/com/rodstone/conversordemonedas/Principal.java
+++ src/com/rodstone/conversordemonedas/Principal.java
@@ -1,7 +1,16 @@
 package com.rodstone.conversordemonedas;
 
+import java.net.http.HttpRequest;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Conversor de Monedas iniciado");
+
+        //construye una solicitud, al ejecutar esa solicitud con el HttpClient
+        //obtiene la respuesta desde la API.
+        ApiClient apiClient = new ApiClient();
+        HttpRequest request = apiClient.createRequest("USD", "MXN");
+        String jsonResponse = apiClient.sendRequest(request);
+        System.out.println(jsonResponse);
     }
 }
