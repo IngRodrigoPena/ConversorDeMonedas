d8ba177929f3b0e4539b8e9b8acc8bad2b63ffcf
IngRodrigoPena <ingrodrigopena@gmail.com>
2025-05-11 20:56:15 -0600

    metodo createRequest() se encargara de construir una solicitud HTTP

--- src/com/rodstone/conversordemonedas/ApiClient.java
+++ src/com/rodstone/conversordemonedas/ApiClient.java
@@ -1,5 +1,8 @@
 package com.rodstone.conversordemonedas;
 import java.net.http.HttpClient;
+import java.net.URI;
+import java.net.http.HttpRequest;
+
 
 //La clase ApiClient encapsula el cliente HTTP (HttpClient)
 //que usaremos para enviar solicitudes a la API de ExchangeRate.
@@ -21,5 +24,26 @@ public class ApiClient {
         public HttpClient getClient() {
             return client;
         }
+
+        //Este método se encargara de construir una solicitud HTTP (HttpRequest)
+        // a la API, usando una URL con parámetros dinámicos:
+        // moneda base y moneda objetivo.
+
+        //String.format(...): construye la URL usando los códigos de moneda base y destino.
+        //HttpRequest.newBuilder(): crea una solicitud HTTP.
+        //.uri(...): especifica la dirección a la que se enviará la solicitud.
+        //.GET(): es una solicitud de tipo GET (porque no estamos enviando datos, solo solicitándolos).
+        //.build(): finaliza la construcción del objeto.
+
+        public HttpRequest createRequest(String baseCode, String targetCode) {
+               String apiKey = "59fa01b6e1764f26115f52fa";  // API_KEY
+               String uri = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s",
+                                            apiKey, baseCode, targetCode);
+
+            return HttpRequest.newBuilder()
+                              .uri(URI.create(uri))
+                              .GET()
+                              .build();
+       }
 }
 
