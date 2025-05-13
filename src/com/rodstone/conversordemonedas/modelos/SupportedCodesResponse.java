6f60b788665fac673526f284370f960fb6ee4297
IngRodrigoPena <ingrodrigopena@gmail.com>
2025-05-12 18:23:42 -0600

    Obtiene de la API todas las monedas disónibles

---
+++ src/com/rodstone/conversordemonedas/modelos/SupportedCodesResponse.java
@@ -0,0 +1,19 @@
+package com.rodstone.conversordemonedas.modelos;
+
+import java.util.List;
+
+// usando la URI GET https://v6.exchangerate-api.com/v6/YOUR_API_KEY/codes
+//Obtendra desde la API todas las monedas disponibles
+//para mostrarle al usaurio un listado actualizado y dinamico
+public class SupportedCodesResponse {
+    private String result;
+    private List<List<String>> suportedCodes;
+
+    public String getResult() {
+        return result;
+    }
+
+    public List<List<String>> getSuportedCodes() {
+        return suportedCodes;
+    }
+}
